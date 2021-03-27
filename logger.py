abf84d85aac33af71a6e7359c69c9cbe3521c176
Sebastian <sebastian.ptz@outlook.com>
2021-03-27 23:39:35 +0100

    notify sends message to channel

--- logger.py
+++ logger.py
@@ -11,11 +11,9 @@ class Logger:
 
     def log(self, label, message):
         #channel = #support of Fachschaft 4
-        channel = Client.get_channel(self ,825376458276339713)
         message = f"[{label}] {message}"
         
         #send message to channel logfile and console
-        await channel.send(f"{message}\n")
         self.log_file.write(f"{message}\n")
         self.log_file.flush()
         print(message)
@@ -25,3 +23,7 @@ class Logger:
 
     def error(self, message):
         self.log("Error", message)
+
+    async def notify(self, message):
+        channel = self.parent.get_channel(820281018263142412)
+        await channel.send(f"{message}\n")
